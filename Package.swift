d63ac163ac4a3a18f18232d9e5f29e0e691346b4
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-15 09:45:24 -0500

    Adding monorepo structure

--- Package.swift
+++ Package.swift
@@ -4,15 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "NekoCore",
+    name: "NekoCommandLineInterface",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "NekoCore",
+            name: "NekoCommandLineInterface/NekoCore",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]

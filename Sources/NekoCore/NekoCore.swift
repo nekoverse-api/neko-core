1d180db3001891d5ac8b097df3d3392763492b20
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-15 23:37:54 -0500

    Adding load method according to extension

--- Sources/NekoCore/NekoCore.swift
+++ Sources/NekoCore/NekoCore.swift
@@ -45,4 +45,47 @@ public struct NekoFileLoader {
             throw error
         }
     }
+
+    public static func load<T>(_ type: T.Type, fileName: String) throws -> T
+    where T: Decodable {
+        let pathExtension = getPathExtension(fileName)
+
+        if isJson(pathExtension) {
+            return try self.loadJson(type, fileName: fileName)
+        }
+
+        if isToml(pathExtension) {
+            return try self.loadToml(type, fileName: fileName)
+        }
+
+        if isYaml(pathExtension) {
+            return try self.loadYaml(type, fileName: fileName)
+        }
+
+        throw ConfigLoaderError.UnsupportedFormatError
+    }
+
+    public static func getPathExtension(_ path: String) -> String {
+        let pathExtension = URL(string: path)?.pathExtension ?? "neko"
+        return pathExtension.lowercased()
+    }
+
+    public static func isJson(_ pathExtension: String) -> Bool {
+        let EXTENSIONS = ["json", "jsonc"]
+        return EXTENSIONS.contains { $0.isEqual(pathExtension) }
+    }
+
+    public static func isToml(_ pathExtension: String) -> Bool {
+        let EXTENSIONS = ["toml", "neko"]
+        return EXTENSIONS.contains { $0.isEqual(pathExtension) }
+    }
+
+    public static func isYaml(_ pathExtension: String) -> Bool {
+        let EXTENSIONS = ["yaml", "yml"]
+        return EXTENSIONS.contains { $0.isEqual(pathExtension) }
+    }
+}
+
+public enum ConfigLoaderError: Error {
+    case UnsupportedFormatError
 }
--- Tests/NekoCoreTests/NekoConfigLoad.swift
+++ Tests/NekoCoreTests/NekoConfigLoad.swift
@@ -4,9 +4,81 @@ import Testing
 
 @Suite
 class NekoConfigSuite {
-    @Test func testLoadTomlConfig() throws {
-        let fileName = "./Tests/Data/Config/Basic.neko.json"
-        let config = try NekoFileLoader.loadJson(NekoConfig.self, fileName: fileName)
+    @Test func testLoadBasicJsonConfig() throws {
+        let path = "./Tests/Data/Config/Basic/Basic.neko.json"
+        let config = try NekoFileLoader.loadJson(NekoConfig.self, fileName: path)
+
+        #expect("neko@v1.0.0".isEqual(config.version))
+    }
+    @Test func testLoadBasicTomlConfig() throws {
+        let path = "./Tests/Data/Config/Basic/Basic.neko.toml"
+        let config = try NekoFileLoader.loadToml(NekoConfig.self, fileName: path)
+
+        #expect("neko@v1.0.0".isEqual(config.version))
+    }
+    @Test func testLoadBasicYamlConfig() throws {
+        let path = "./Tests/Data/Config/Basic/Basic.neko.yaml"
+        let config = try NekoFileLoader.loadYaml(NekoConfig.self, fileName: path)
+
+        #expect("neko@v1.0.0".isEqual(config.version))
+    }
+
+    @Test(
+        arguments: zip(
+            [
+                "./Tests/Data/Config/Basic/Basic.neko.json",
+                "./Tests/Data/Config/Basic/Basic.neko.JSON",
+                "./Tests/Data/Config/Basic/Basic.neko.JsoN",
+                "./Tests/Data/Config/Basic/Basic.neko.toml",
+                "./Tests/Data/Config/Basic/Basic.neko.TOML",
+                "./Tests/Data/Config/Basic/Basic.neko.TomL",
+                "./Tests/Data/Config/Basic/Basic.neko",
+                "./Tests/Data/Config/Basic/Basic.neko.yaml",
+                "./Tests/Data/Config/Basic/Basic.neko.YAML",
+                "./Tests/Data/Config/Basic/Basic.neko.YamL",
+            ],
+            [
+                "json",
+                "json",
+                "json",
+                "toml",
+                "toml",
+                "toml",
+                "neko",
+                "yaml",
+                "yaml",
+                "yaml",
+            ]
+        )
+    )
+    func testLoadBasicTomlConfig(path: String, expectedPathExtension: String) throws {
+        let pathExtension = NekoFileLoader.getPathExtension(path)
+
+        #expect(expectedPathExtension.isEqual(pathExtension))
+    }
+
+    @Test
+    func testIsJson() {
+        #expect(NekoFileLoader.isJson("json"))
+        #expect(NekoFileLoader.isJson("jsonc"))
+        #expect(NekoFileLoader.isJson("gary") == false)
+
+        #expect(NekoFileLoader.isToml("toml"))
+        #expect(NekoFileLoader.isToml("neko"))
+        #expect(NekoFileLoader.isToml("gary") == false)
+
+        #expect(NekoFileLoader.isYaml("yaml"))
+        #expect(NekoFileLoader.isYaml("yml"))
+        #expect(NekoFileLoader.isYaml("gary") == false)
+    }
+
+    @Test(arguments: [
+        "./Tests/Data/Config/Basic/Basic.neko.json",
+        "./Tests/Data/Config/Basic/Basic.neko.toml",
+        "./Tests/Data/Config/Basic/Basic.neko.yaml",
+    ])
+    func testLoadBasicTomlConfig(path: String) throws {
+        let config = try NekoFileLoader.load(NekoConfig.self, fileName: path)
 
         #expect("neko@v1.0.0".isEqual(config.version))
     }

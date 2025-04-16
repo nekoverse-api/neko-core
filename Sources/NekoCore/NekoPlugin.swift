abfddfc307d0f5f5f6501f152541352038196d62
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-15 23:56:48 -0500

    Adding loader plugins

---
+++ Sources/NekoCore/NekoPlugin.swift
@@ -0,0 +1,24 @@
+//
+// Neko Pluggins (Loader)
+//
+public protocol NekoLoaderPlugin {
+    func load<T>(_ type: T.Type, fileName: String) async throws -> T where T: Decodable
+}
+
+//
+// Plugin that loads JSON, JSONC, TOML, NEKO, YAML, YML, CSV files
+//
+public struct NekoFileLoaderPlugin: NekoLoaderPlugin {
+    public func load<T>(_ type: T.Type, fileName: String) async throws -> T
+    where T: Decodable {
+        return try NekoFileLoader.load(type, fileName: fileName)
+    }
+}
+
+public struct NekoGitFriendlyLoaderPlugin: NekoLoaderPlugin {
+    public func load<T>(_ type: T.Type, fileName: String) async throws -> T
+    where T: Decodable {
+        // TODO: Loads using folders partialy
+        return try NekoFileLoader.load(type, fileName: fileName)
+    }
+}

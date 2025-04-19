334885ec15a7723a5b8746eae62668cb9f9eb106
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-19 08:57:03 -0500

    Moving some functins into the shared module

--- Sources/NekoCore/NetworkPlugin/AlamofireNekoNetwork.swift
+++ Sources/NekoCore/NetworkPlugin/AlamofireNekoNetwork.swift
@@ -84,7 +84,7 @@ extension NekoCore {
             var tslCipherSuite = ""
 
             let httpVersion = metric.networkProtocolName ?? ""
-            let domainResolutionProtocol = getDomainResolutionProtocolName(
+            let domainResolutionProtocol = Shared.getDomainResolutionProtocolName(
                 metric.domainResolutionProtocol)
 
             let local = NekoResponseMetadataNetworkServer(
@@ -95,11 +95,11 @@ extension NekoCore {
             )
 
             if let protocolVersion = metric.negotiatedTLSProtocolVersion {
-                tlsProtocolVersion = getProtocolVersionName(protocolVersion)
+                tlsProtocolVersion = Shared.getProtocolVersionName(protocolVersion)
             }
 
             if let cipherSuite: tls_ciphersuite_t = metric.negotiatedTLSCipherSuite {
-                tslCipherSuite = getCipherSuiteName(cipherSuite)
+                tslCipherSuite = Shared.getCipherSuiteName(cipherSuite)
             }
 
             return NekoResponseMetadata(
@@ -116,61 +116,5 @@ extension NekoCore {
             )
         }
 
-        public static func getProtocolVersionName(_ version: tls_protocol_version_t) -> String {
-            return switch version {
-            case .TLSv10: "TLSv10"
-            case .TLSv11: "TLSv11"
-            case .TLSv12: "TLSv12"
-            case .TLSv13: "TLSv13"
-            case .DTLSv10: "DTLSv10"
-            case .DTLSv12: "DTLSv12"
-            default: "UNKNOWN"
-            }
-        }
-
-        public static func getCipherSuiteName(_ ciphersuite: tls_ciphersuite_t) -> String {
-            return switch ciphersuite {
-            case .RSA_WITH_3DES_EDE_CBC_SHA: "RSA_WITH_3DES_EDE_CBC_SHA"
-            case .RSA_WITH_AES_128_CBC_SHA: "RSA_WITH_AES_128_CBC_SHA"
-            case .RSA_WITH_AES_256_CBC_SHA: "RSA_WITH_AES_256_CBC_SHA"
-            case .RSA_WITH_AES_128_GCM_SHA256: "RSA_WITH_AES_128_GCM_SHA256"
-            case .RSA_WITH_AES_256_GCM_SHA384: "RSA_WITH_AES_256_GCM_SHA384"
-            case .RSA_WITH_AES_128_CBC_SHA256: "RSA_WITH_AES_128_CBC_SHA256"
-            case .RSA_WITH_AES_256_CBC_SHA256: "RSA_WITH_AES_256_CBC_SHA256"
-            case .ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA: "ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA"
-            case .ECDHE_ECDSA_WITH_AES_128_CBC_SHA: "ECDHE_ECDSA_WITH_AES_128_CBC_SHA"
-            case .ECDHE_ECDSA_WITH_AES_256_CBC_SHA: "ECDHE_ECDSA_WITH_AES_256_CBC_SHA"
-            case .ECDHE_RSA_WITH_3DES_EDE_CBC_SHA: "ECDHE_RSA_WITH_3DES_EDE_CBC_SHA"
-            case .ECDHE_RSA_WITH_AES_128_CBC_SHA: "ECDHE_RSA_WITH_AES_128_CBC_SHA"
-            case .ECDHE_RSA_WITH_AES_256_CBC_SHA: "ECDHE_RSA_WITH_AES_256_CBC_SHA"
-            case .ECDHE_ECDSA_WITH_AES_128_CBC_SHA256: "ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
-            case .ECDHE_ECDSA_WITH_AES_256_CBC_SHA384: "ECDHE_ECDSA_WITH_AES_256_CBC_SHA384"
-            case .ECDHE_RSA_WITH_AES_128_CBC_SHA256: "ECDHE_RSA_WITH_AES_128_CBC_SHA256"
-            case .ECDHE_RSA_WITH_AES_256_CBC_SHA384: "ECDHE_RSA_WITH_AES_256_CBC_SHA384"
-            case .ECDHE_ECDSA_WITH_AES_128_GCM_SHA256: "ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
-            case .ECDHE_ECDSA_WITH_AES_256_GCM_SHA384: "ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
-            case .ECDHE_RSA_WITH_AES_128_GCM_SHA256: "ECDHE_RSA_WITH_AES_128_GCM_SHA256"
-            case .ECDHE_RSA_WITH_AES_256_GCM_SHA384: "ECDHE_RSA_WITH_AES_256_GCM_SHA384"
-            case .ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256: "ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
-            case .ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256:
-                "ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
-            case .AES_128_GCM_SHA256: "AES_128_GCM_SHA256"
-            case .AES_256_GCM_SHA384: "AES_256_GCM_SHA384"
-            default: "UNKNOWN"
-            }
-        }
-
-        public static func getDomainResolutionProtocolName(
-            _ domainResolutionProtocol: URLSessionTaskMetrics.DomainResolutionProtocol
-        ) -> String {
-            return switch domainResolutionProtocol {
-            case .unknown: "UNKNOWN"
-            case .udp: "UDP"
-            case .tcp: "TCP"
-            case .tls: "TLS"
-            case .https: "HTTPS"
-            default: "UNKNOWN"
-            }
-        }
     }
 }
--- Tests/NekoCoreTests/NetworkPluginTests/SharedTests.swift
+++ Tests/NekoCoreTests/NetworkPluginTests/SharedTests.swift
@@ -59,21 +59,47 @@ struct SharedTests {
 
     @Test
     func testDurationBetweenDates() async throws {
+        let durationBetweenDates = NekoCore.Shared.durationBetweenDates
+
         let start = Date()
         let end = start + 1
 
-        let duration = NekoCore.Shared.durationBetweenDates(start, end)
+        let duration = durationBetweenDates(start, end)
         #expect(duration.components.seconds >= 1)
         #expect(duration.components.attoseconds == 0)
     }
 
     @Test
     func testDurationBetweenDatesEdgeCases() async throws {
+        let durationBetweenDates = NekoCore.Shared.durationBetweenDates
         let start = Date()
         let end = start + 1
 
-        #expect(NekoCore.Shared.durationBetweenDates(nil, nil) == Duration.seconds(0))
-        #expect(NekoCore.Shared.durationBetweenDates(start, nil) == Duration.seconds(0))
-        #expect(NekoCore.Shared.durationBetweenDates(nil, end) == Duration.seconds(0))
+        #expect(durationBetweenDates(nil, nil) == Duration.seconds(0))
+        #expect(durationBetweenDates(start, nil) == Duration.seconds(0))
+        #expect(durationBetweenDates(nil, end) == Duration.seconds(0))
+    }
+
+    @Test
+    func testGetProtocolVersionName() async throws {
+        #expect("TLSv13".isEqual(NekoCore.Shared.getProtocolVersionName(.TLSv13)))
+        #expect("TLSv12".isEqual(NekoCore.Shared.getProtocolVersionName(.TLSv12)))
+        #expect("DTLSv12".isEqual(NekoCore.Shared.getProtocolVersionName(.DTLSv12)))
+    }
+
+    @Test
+    func testGetCipherSuiteName() async throws {
+        let getCipherSuiteName = NekoCore.Shared.getCipherSuiteName
+        #expect("AES_256_GCM_SHA384".isEqual(getCipherSuiteName(.AES_256_GCM_SHA384)))
+        #expect("RSA_WITH_AES_256_CBC_SHA".isEqual(getCipherSuiteName(.RSA_WITH_AES_256_CBC_SHA)))
+    }
+
+    @Test func testGetDomainResolutionProtocolName() async throws {
+        let getDomainResolutionProtocolName = NekoCore.Shared.getDomainResolutionProtocolName
+
+        #expect("UDP".isEqual(getDomainResolutionProtocolName(.udp)))
+        #expect("TCP".isEqual(getDomainResolutionProtocolName(.tcp)))
+        #expect("HTTPS".isEqual(getDomainResolutionProtocolName(.https)))
+        #expect("UNKNOWN".isEqual(getDomainResolutionProtocolName(.unknown)))
     }
 }

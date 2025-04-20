c1c0a23e27b89b3ed51ffdd49cd5010e8f8daf6c
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-19 21:05:16 -0500

    Moving code

--- Sources/NekoCore/NekoCore.swift
+++ Sources/NekoCore/NekoCore.swift
@@ -10,6 +10,11 @@ import Mustache
 import Rainbow
 import SwiftyJSON
 
+public struct NekoCore {
+    public static let version = "0.0.1"
+    public init() {}
+}
+
 //
 // NekoLoaderPlugin is used to load configuration
 //
@@ -29,9 +34,4 @@ public protocol NekoExecutorPlugin {
 //
 public protocol NekoTesterPlugin {
     func test(_ script: String, _ response: NekoResponse) async throws -> NekoTestResponse
-}
-
-public struct NekoCore {
-    public static let version = "0.0.1"
-    public init() {}
 }
